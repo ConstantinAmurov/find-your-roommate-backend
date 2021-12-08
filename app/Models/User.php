191d12d77fda945505c7c53c6a9659b91cd6af17
CostelMD <amurov.constantin@gmail.com>
2021-12-08 12:31:26 +0100

    fix with http link

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,7 +56,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function sendPasswordResetNotification($token)
     {
-        $url = 'https://find-your-roommate-app.herokuapp.com/reset-password?token=' . $token ;
+        $url = 'http://find-your-roommate-app.herokuapp.com/reset-password?token=' . $token ;
         $this->notify(new ResetPasswordNotification($url));
     }
 
